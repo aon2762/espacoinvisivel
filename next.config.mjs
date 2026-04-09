0dcc1babac8816f4bc95ec99b9a42e98693097af
aon2762 <34953155@student.murdoch.edu.au>
2026-04-10 02:39:39 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  trailingSlash: true,     // ← Change to true if you want / at the end
+  reactStrictMode: true,
+};
+
+export default nextConfig;
